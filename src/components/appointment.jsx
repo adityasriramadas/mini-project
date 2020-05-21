31bcf8df2ddc09af49e6ff9f57f184712e4d2830
nikhil-reddy05 <56714003+nikhil-reddy05@users.noreply.github.com>
2020-05-21 22:37:20 +0530

    fixed date range in booking

--- src/components/appointment.jsx
+++ src/components/appointment.jsx
@@ -1,41 +1,104 @@
-import React from 'react';
+import React from "react";
+import { Cascader } from "antd";
 
 import "./appointment.css";
+var date = new Date();
+date = date.toJSON().slice(0, 10);
+var today = date.slice(0, 4) + "-" + date.slice(5, 7) + "-" + date.slice(8, 10);
+var result = new Date();
+result.setDate(result.getDate() + 7);
+result = result.toJSON().slice(0, 10);
+var max_date =
+  result.slice(0, 4) + "-" + result.slice(5, 7) + "-" + result.slice(8, 10);
+const options = [
+  {
+    value: "9:00 AM",
+    label: "9:00 AM",
+  },
+  {
+    value: "10:00 AM",
+    label: "10:00 AM",
+  },
+  {
+    value: "11:00 AM",
+    label: "11:00 AM",
+  },
+  {
+    value: "12:00 PM",
+    label: "12:00 PM",
+  },
+  {
+    value: "02:00 PM",
+    label: "02:00 PM",
+  },
+  {
+    value: "03:00 PM",
+    label: "03:00 PM",
+  },
+];
+class Appointment extends React.Component {
+  onChange(value) {
+    console.log(value);
+  }
+  render() {
+    console.log(today);
+    return (
+      <div class="appointment_sec">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-6 col-lg-5 offset-lg-7 offset-md-6">
+              <div class="appoint_ment_form">
+                <h2>Book an Appointment</h2>
+                <p>Please fill details to book an appointment!</p>
+                <hr />
+                <form>
+                  <fieldset>
+                    <input
+                      type="text"
+                      ref="name"
+                      placeholder="Name of patients"
+                      required="required"
+                    />
 
+                    <input
+                      placeholder="Age"
+                      type="number"
+                      min="1"
+                      max="99"
+                      required="required"
+                    ></input>
+                    <input
+                      type="tel"
+                      placeholder="Mobile Number"
+                      pattern="[0-9]{10}"
+                      required="required"
+                    ></input>
+                    <input
+                      id="datepicker-disable-past"
+                      type="date"
+                      placeholder="DDMMYYYY"
+                      min={today}
+                      max={max_date}
+                      required="required"
+                    />
 
-class Appointment extends React.Component{
-    
-
-    render(){
-        return(
-            <div class="appointment_sec">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-md-6 col-lg-5 offset-lg-7 offset-md-6">
-                            <div class="appoint_ment_form">
-                                <h2>Book an Appointment</h2>
-                                <p>Please fill details to book an appointment!</p>
-                                <hr/>
-                                <form >
-                                    <fieldset>
-                                    <input type="text" ref="name" placeholder="Name of patients"  required="required"/>
-                                   
-                                    <input placeholder='Age' type="number" min="1" max="99" required="required"></input>
-                                    <input type="tel" placeholder="Mobile Number" pattern='[0-9]{10}'  required="required"></input>
-                                    <input id="datepicker-disable-past" type="date" placeholder='DDMMYYYY' min='2020-04-16' required="required"/>
-                                    
-                                    <button type="submit" class="btn">Get Your Free Appointment</button>
-                                    </fieldset>
-                                </form>
-                            </div>
-                           
-                        </div>
-                    </div>
-                </div>
-             
+                    <button type="submit" class="btn">
+                      Get Your Free Appointment
+                    </button>
+                  </fieldset>
+                </form>
+                <Cascader
+                  options={options}
+                  onChange={this.onChange}
+                  changeOnSelect
+                />
+              </div>
             </div>
-        );
-    }
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Appointment;
--- src/index.js
+++ src/index.js
@@ -1,52 +1,47 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import NavigationBar from './components/nav';
-import Footer from './components/Footer';
-import About from './components/About';
-import Login from './components/login';
-import Appointment from './components/appointment';
-import Signup from './components/signup';
-import Home from './components/Home';
+import React from "react";
+import ReactDOM from "react-dom";
+import NavigationBar from "./components/nav";
+import Footer from "./components/Footer";
+import About from "./components/About";
+import Login from "./components/login";
+import Appointment from "./components/appointment";
+import Signup from "./components/signup";
+import Home from "./components/home";
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'font-awesome/css/font-awesome.min.css';
-import {BrowserRouter,Switch,Route} from 'react-router-dom';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "font-awesome/css/font-awesome.min.css";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
-class MyNavbar extends React.Component{
+class MyNavbar extends React.Component {
   render() {
-      return (
-        <div> 
-          <BrowserRouter>
-            <NavigationBar/>
-            
-              <Switch>
-                <Route exact path='/' component={Home}></Route>
-                <Route path='/appointment'>
-                  <Appointment/>
-                </Route>
+    return (
+      <div>
+        <BrowserRouter>
+          <NavigationBar />
 
-                <Route path='/About'>
-                  <About/>
-                </Route>
-                
-                <Route path='/login'>
-                  <Login/>
-                </Route>
+          <Switch>
+            <Route exact path="/" component={Home}></Route>
+            <Route path="/appointment">
+              <Appointment />
+            </Route>
 
-                <Route path='/signup'>
-                  <Signup/>
-                </Route>
-                
-            </Switch>
-            <Footer/>
-          </BrowserRouter>
-          
+            <Route path="/About">
+              <About />
+            </Route>
 
-        </div>
-      
-      
-      );
+            <Route path="/login">
+              <Login />
+            </Route>
+
+            <Route path="/signup">
+              <Signup />
+            </Route>
+          </Switch>
+          <Footer />
+        </BrowserRouter>
+      </div>
+    );
   }
 }
 
-ReactDOM.render(<MyNavbar />, document.getElementById('root'));
+ReactDOM.render(<MyNavbar />, document.getElementById("root"));
