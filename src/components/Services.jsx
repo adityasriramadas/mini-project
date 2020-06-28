d7d784f40eb6b5e397843185a2f8d738c3f0daec
nikhil-reddy05 <56714003+nikhil-reddy05@users.noreply.github.com>
2020-06-28 14:41:00 +0530

    read and display data initialised

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,26 +1,43 @@
 import React, { Component } from "react";
 import { Avatar } from "antd";
 import { connect } from "react-redux";
-import "./Services.css"
+import "./Services.css";
 
+import firebase from "../firebase";
+import "firebase/firestore";
+require("firebase/firestore");
 
+const db = firebase.firestore();
 class Services extends Component {
   constructor() {
     super();
+    this.state = {
+      name: "",
+    };
     this.display = this.display.bind(this);
-   
   }
-     
-   
-    display = () => {
-      db.collection("patients")
-              .doc(cc.state.doa)
-              .collection("Dr Nikhil")
-              .doc("09:00AM")
-               .then((doc)=>({
-                console.log(doc.data())
-      })
-    }
+  componentDidMount() {
+    this.display();
+  }
+  display = () => {
+    const cc = this;
+    db.collection("patients")
+      .doc("2020-07-01")
+      .collection("Dr Nikhil")
+      .doc("09:00 AM")
+      .get()
+      .then(function (doc) {
+        if (doc.exists) {
+          var data = doc.data();
+          console.log(doc.data());
+          cc.setState({
+            name: data.patientDetails.name,
+          });
+        }
+      });
+  };
+
+  render() {
     return (
       <div className="selected-driver-details pt-3">
         <h3 className="choose">Your Driver</h3>
@@ -32,10 +49,8 @@ class Services extends Component {
             <div className="row ml-3 mt-3">
               <div className="col-4">Name</div>
               <div className="col-1">:</div>
-              <div className="col-6">{name}</div>
+              <div className="col-6">{this.state.name}</div>
             </div>
-            
-           
           </form>
         </div>
         <div className="d-flex flex-row justify-content-center my-2">
@@ -49,10 +64,7 @@ class Services extends Component {
 }
 
 const mapStateToProps = (state) => ({
-  name: state.patientDetails.name,
- 
+  userDetails: state.loginStatus.userDetails,
 });
 
 export default connect(mapStateToProps, null)(Services);
-
-
